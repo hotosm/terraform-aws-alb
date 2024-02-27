d9baae5688972b9101e5c000c5ad286c2cca8163
eternaltyro <eternaltyro@protonmail.ch>
2024-02-27 14:07:58 +0000

    Fix typo

--- outputs.tf
+++ outputs.tf
@@ -23,7 +23,7 @@ output "target_group_arn" {
   value       = aws_lb_target_group.main.arn
 }
 
-output "target_group_arn_suffix {
+output "target_group_arn_suffix" {
   description = "ARN of the load balancer target group"
   value       = aws_lb_target_group.main.arn_suffix
 }
